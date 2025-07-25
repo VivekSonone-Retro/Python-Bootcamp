8a840760b4fbe160d3bdd33a5ae5c620cca77108
VivekSonone-Retro <vivek0sonone@gmail.com>
2025-07-25 19:16:13 +0530

    Modified the code and added a function in it

--- Assignment1.py
+++ Assignment1.py
@@ -1,7 +1,15 @@
-#% messages that hs to print
+# messages that has to print
 
 print ("Hello World")
 print ("hii my name is Vivek")
 print ("I enrolled for a 30 days bootcamo")
 print ("Which is organized by Wisdom Sprout")
 print ("it is a leading training institute in pune")
+
+#function to print my name and age
+# Define the function
+def print_name_and_age(name, age): #This function is use to call the Name and Age
+    
+    print(f"Hello, my name is {name} and I am {age} years old.")
+
+print_name_and_age("Vivek", 21)
